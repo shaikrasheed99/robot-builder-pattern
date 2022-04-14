994cf6e90983584e0bb97af805ff09d26308b3eb
rasheed <rasheed.shaik@thoughtworks.com>
2022-04-14 11:41:06 +0530

    Refactor increase the number of arguments in robot constructor

--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RobotTest {
+
     @Test
     void shouldCreateRobotWithTwoArms() {
         Robot robot = new Robot(2, 0);
