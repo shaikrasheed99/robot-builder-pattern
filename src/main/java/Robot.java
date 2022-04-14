0ad0d4542d93104bff84ea0ce3e13b5f92342e0a
rasheed <rasheed.shaik@thoughtworks.com>
2022-04-14 11:38:25 +0530

    Add logic to create a robot with sensors

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -1,11 +1,17 @@
 public class Robot {
     private final int numberOfArms;
+    private final int numberOfSensors;
 
-    public Robot(int numberOfArms) {
+    public Robot(int numberOfArms, int numberOfSensors) {
         this.numberOfArms = numberOfArms;
+        this.numberOfSensors = numberOfSensors;
     }
 
     public int getArms() {
         return numberOfArms;
+    }
+
+    public int getSensors() {
+        return numberOfSensors;
     }
 }
--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -5,8 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RobotTest {
     @Test
     void shouldCreateRobotWithTwoArms() {
-        Robot robot = new Robot(2);
+        Robot robot = new Robot(2, 0);
 
         assertEquals(2, robot.getArms());
+    }
+
+    @Test
+    void shouldCreateRobotWithTenSensors() {
+        Robot robot = new Robot(2, 10);
+
+        assertEquals(10, robot.getSensors());
     }
 }
